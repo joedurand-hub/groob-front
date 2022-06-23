d46745de2632cdc3782c5f006095e4f2e653cce5
joedurand-hub <53331000+joedurand-hub@users.noreply.github.com>
2022-06-23 01:24:39 -0300

    naming Images

--- src/pages/Feed/Feed.jsx
+++ src/pages/Feed/Feed.jsx
@@ -6,7 +6,7 @@ const Feed = () => {
  
   return (
     <Layout>
-      <Image src={"https://static.pintzap.com/img/pics/t/600/1655624477_creare-mi-propia-red-social-con-juegos-de-azar-y-mujerzuelas.png"} width={400} height={400}/>
+      <Image src={"https://static.pintzap.com/img/pics/t/600/1655624477_creare-mi-propia-red-social-con-juegos-de-azar-y-mujerzuelas.png"} width={400} height={400} alt="Image"/>
     </Layout>
   );
 };
