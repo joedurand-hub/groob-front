77dc2d3b65d9543f84797d01f4739289e1cfd221
Joel <joeljuliandurand@gmail.com>
2022-08-23 15:52:31 -0300

    socket in [id]

--- src/pages/messages/[id].jsx
+++ src/pages/messages/[id].jsx
@@ -5,7 +5,6 @@ import SendMessage from "../../components/Conversation/SendMessage/SendMessage";
 import Message from "../../components/Conversation/Message/Message";
 import GoBack from "../../components/GoBack/Back";
 import Conversation from "../../components/Conversation/Conversation";
-import useAuthPost from "../../hooks/useAuthPost";
 import axios from "axios";
 import InputEmoji from "react-input-emoji";
 import { io } from "socket.io-client";
@@ -14,9 +13,10 @@ const Messages = ({ datas }) => {
   const token = getCookie("authToken");
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState("");
-  const [sendMessage, setSendMessage] = useState(null);
-  const socket = useRef();
-
+  const [submitMessage, setSubmitMessage] = useState({})
+  const [reciveMessage, setReciveMessage] = useState(null);
+  const [onlineUsers, setOnlineUsers] = useState({});
+  // const socket = useRef();
   const reciverId = datas.chat.members.find(id => id !== datas.myId)
 
   useEffect(() => {
@@ -34,12 +34,46 @@ const Messages = ({ datas }) => {
     } catch (error) {
       console.log(error);
     }
-  }, [sendMessage]);
+  }, [reciveMessage]);
 
   const handleMessage = (newMessage) => {
     setNewMessage(newMessage);
   };
+
+    const socketMessage = {
+      reciverId,
+      chatId: datas?.chat._id,
+      senderId: messages.myId,
+      text: newMessage,
+  }
+  // console.log("Nuevo mensaje: ", newMessage)
+  // console.log("Mensaje enviado: ", submitMessage)
   
+  // useEffect(() => {
+  //   socket.current = io("http://localhost:8080");
+  //   socket.current.emit("newUserAdded", messages.myId)
+  //   socket.current.on("getUsers", (allUsers) => {
+  //     setOnlineUsers(allUsers)
+  //   })
+  // }, [onlineUsers]);
+  
+  console.log("online users:", onlineUsers)
+  
+  // console.log("Mensaje enviado al socket: ", socketMessage)
+    // useEffect(() => {
+    //   socket.current = io("http://localhost:8080");
+    //     socket.current.emit("newMessage", socketMessage);
+    //   }, [submitMessage]);
+
+    // useEffect(() => {
+    //   socket.current = io("http://localhost:8080");
+    //   socket.current.on("reciveMessage", (newMessage) => {
+    //     setReciveMessage(newMessage)
+    //   })
+    // }, [reciveMessage]);
+  // console.log("Recibo desde el socket: ", reciveMessage)
+
+
   const handleSendMessage = async () => {
     try {
       const { data } = await axios.post(
@@ -52,28 +86,13 @@ const Messages = ({ datas }) => {
         { headers: { authToken: token } },
         { withCredentials: true }
         );
-        setSendMessage(data)
+        setSubmitMessage(data)
         setNewMessage("");
     } catch (error) {
       console.log("error: ", error);
     }
   };
 
-  const socketMessage = {
-    reciverId,
-    chatId: datas?.chat._id,
-    senderId: messages.myId,
-    text: newMessage,
-  }
-
-  useEffect(() => {
-      socket.current = io("http://localhost:8080");
-      socket.current.emit("sendMessage", socketMessage);
-      socket.current.on("recivedMessage", (newMessage) => {
-        setSendMessage(newMessage)
-      })
-  }, [newMessage]);
-  console.log(sendMessage)
   
   return (
     <div>
