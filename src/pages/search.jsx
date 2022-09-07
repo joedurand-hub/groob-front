fcf72ea36886ed593b6bbf5ca4561ec4612781cc
Joel <joeljuliandurand@gmail.com>
2022-09-07 18:09:02 -0300

    deleted streaming route and fix search route

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -3,52 +3,50 @@ import Icon from "../components/Icon/Icon";
 import Nav from "../components/Nav/Nav";
 import CreatePost from "../components/CreatePost/CreatePost";
 import NavItem from "../components/NavItem/NavItem";
-import Modal from "../components/Modal/Modal"
-import { useModal } from "../hooks/useModal"
-import { getCookie } from "cookies-next";
+import Modal from "../components/Modal/Modal";
+import { useModal } from "../hooks/useModal";
 import { BiHome } from "react-icons/bi";
 import { RiSearchEyeFill } from "react-icons/ri";
-import { BsFillPlusCircleFill } from "react-icons/bs"; 
+import { BsFillPlusCircleFill } from "react-icons/bs";
 import { BiUser, BiChat } from "react-icons/bi";
-import { MdOutlineNotificationsNone } from "react-icons/md"
+import { MdOutlineNotificationsNone } from "react-icons/md";
 import OpenModalPost from "../components/CreatePost/OpenModalPost/OpenModalPost";
 
 const Search = () => {
   const [isOpenModalPost, openModalPost, closeModalPost] = useModal(false);
   return (
     <Layout
-    menuItem={
-      <Icon>
-      <MdOutlineNotificationsNone />
-    </Icon>
-    }
+      menuItem={
+        <Icon>
+          <MdOutlineNotificationsNone />
+        </Icon>
+      }
       nav={
         <>
           <Nav>
-          <NavItem path="/feed">
-            <BiHome/>
-          </NavItem>
-          <NavItem path="/search">
-            <RiSearchEyeFill />
-          </NavItem>
-           <OpenModalPost openModalPost={openModalPost}/>
-          <NavItem path="/messages">
-            <BiChat />
-          </NavItem>
-        <NavItem path="/user">
-        <BiUser />
-      </NavItem>
-        </Nav>
+            <NavItem path="/feed">
+              <BiHome />
+            </NavItem>
+            <NavItem path="/search">
+              <RiSearchEyeFill />
+            </NavItem>
+            <OpenModalPost openModalPost={openModalPost} />
+            <NavItem path="/messages">
+              <BiChat />
+            </NavItem>
+            <NavItem path="/user">
+              <BiUser />
+            </NavItem>
+          </Nav>
         </>
       }
-    
-  >
-    <Modal isOpen={isOpenModalPost} closeModal={closeModalPost}>
-      <CreatePost closeModal={closeModalPost}/>
-    </Modal>
-    hola gente linda <br/><br/><br/>
-        </Layout>
-  )
-}
+    >
+      <Modal isOpen={isOpenModalPost} closeModal={closeModalPost}>
+        <CreatePost closeModal={closeModalPost} />
+      </Modal>
+      <input type="search" placeholder="Descubre publicaciones"/>
+    </Layout>
+  );
+};
 
-export default Search
+export default Search;
