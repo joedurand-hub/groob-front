d3a824ea402fd66d65cb176bd82dfeadb126308e
Joel <joeljuliandurand@gmail.com>
2022-09-25 20:50:24 -0300

    changed theme state in true

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -2,7 +2,7 @@ import { createContext, useEffect, useState } from "react";
 const ThemeContext = createContext();
 
 const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState(false);
+  const [theme, setTheme] = useState(true);
 
   useEffect(() => {
     let data = window.localStorage.getItem("theme");
