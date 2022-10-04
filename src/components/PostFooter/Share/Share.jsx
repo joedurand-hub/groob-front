f6bccc6af745e6926a2064fd8c3f8d0d9a68aa0c
Joel <joeljuliandurand@gmail.com>
2022-10-03 23:44:40 -0300

    news share icon

--- src/components/PostFooter/Share/Share.jsx
+++ src/components/PostFooter/Share/Share.jsx
@@ -1,67 +1,73 @@
-import { useState } from 'react'
-import { IoMdShareAlt } from "react-icons/io"
-import { useEffect } from 'react'
+import { useState } from "react";
+import { IoMdShareAlt } from "react-icons/io";
+import { FaShareSquare } from "react-icons/fa";
+import { RiShareForward2Line } from "react-icons/ri";
+import { useEffect } from "react";
 
-const Share = ({content, price, fileLink, postIdLink, username, description, userLink}) => {
-  const [file, setFile] = useState(null)
+const Share = ({
+  content,
+  price,
+  fileLink,
+  postIdLink,
+  username,
+  description,
+  userLink,
+}) => {
+  const [file, setFile] = useState(null);
 
   // fileLink es un array de imágenes
   // cada posición contiene public_id, secure_url
 
-  const images = fileLink?.map(image => image.secure_url)
+  const images = fileLink?.map((image) => image.secure_url);
   // Necesito iterar sobre el array de imágenes
   // Y a cada una convertirla en blob para luego almacenarla
 
   useEffect(() => {
     async function getFile() {
-      const image = await fetch(`${images[0]}`)
-      const blob = await image.blob()
-      const file = new File([blob], 'Imagen')
-      setFile(file)
+      const image = await fetch(`${images[0]}`);
+      const blob = await image.blob();
+      const file = new File([blob], "Imagen");
+      setFile(file);
     }
-    getFile()
-  }, [])
+    getFile();
+  }, []);
 
-  
   const sharePost = async (data) => {
     if (navigator.share) {
       try {
         await navigator
           .share(data)
-          .then(() =>
-            console.log("Largar alerta de que se está compartiendo")
-          );
+          .then(() => console.log("Largar alerta de que se está compartiendo"));
       } catch (error) {
         console.log(`Ups! No se pudo compartir debido a: ${error}`);
       }
     } else {
-      console.log(
-        "Tu navegador no tiene soporte para esta funcionalidad."
-      );
+      console.log("Tu navegador no tiene soporte para esta funcionalidad.");
     }
-  }
+  };
 
   // Post con foto
   const postWithOutPriceButWithImage = {
     title: username ? username : price,
     text: content ? content : "Groob",
     url: postIdLink,
-  }
-
+  };
 
   // Perfil de usuario
   const user = {
     title: username,
     text: description,
     url: userLink,
-    files: fileLink &&  [file]
-  }
+    files: fileLink && [file],
+  };
 
   return (
     <>
-      <IoMdShareAlt onClick={() => sharePost(postWithOutPriceButWithImage)}/>
+      <IoMdShareAlt onClick={() => sharePost(postWithOutPriceButWithImage)} />
+      <FaShareSquare onClick={() => sharePost(postWithOutPriceButWithImage)} />
+      <RiShareForward2Line onClick={() => sharePost(postWithOutPriceButWithImage)} />
     </>
-  )
-}
+  );
+};
 
-export default Share
+export default Share;
