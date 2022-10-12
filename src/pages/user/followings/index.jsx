f65427e64b210858a21958b00e4daecadbbace48
Joel <joeljuliandurand@gmail.com>
2022-10-12 14:04:37 -0300

    fix index in followings

--- src/pages/user/followings/index.jsx
+++ src/pages/user/followings/index.jsx
@@ -1,36 +1,44 @@
-import Layout from "../../../../components/Layout/Layout";
+import Layout from "../../../components/Layout/Layout";
 import { getCookie } from "cookies-next";
 
-const Followings = ({data}) => {
+const Followings = ({ data }) => {
   return (
-    <Layout>
-      {data && data.followings.map(userId => (
-        <>
-        <h4>id de a quien sigo: {userId}</h4>
-        </>
-      ))}
+    <>
+      <Layout>
+        {data &&
+          data.followings.map((userId) => (
+            <>
+              <h4>id de a quien sigo: {userId}</h4>
+            </>
+          ))}
       </Layout>
-  )
-}
+    </>
+  );
+};
 
 export default Followings;
 
-export async function getServerSideProps({req, res}) {
+export async function getServerSideProps({ req, res }) {
   try {
-    const token = getCookie("authtoken", {req, res});
-    const response = await fetch(`http://localhost:8080/followings`, {
-      headers: { 
-        "authtoken": token 
-      }}, {
-        credentials: 'include'
-      })
-    const data = await response.json()
+    const token = getCookie("authtoken", { req, res });
+    const response = await fetch(
+      `http://localhost:8080/followings`,
+      {
+        headers: {
+          authtoken: token,
+        },
+      },
+      {
+        credentials: "include",
+      }
+    );
+    const data = await response.json();
     return {
       props: {
-        data
+        data,
       },
     };
-  } catch(error) {
-    console.table(error)
+  } catch (error) {
+    console.table(error);
   }
 }
