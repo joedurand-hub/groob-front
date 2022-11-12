03e8857e6b7c7e3f0c0831f8a0af3f052d7a93df
Joel <joeljuliandurand@gmail.com>
2022-11-12 17:49:42 -0300

    webhuk1

--- src/pages/notifications/success.jsx
+++ src/pages/notifications/success.jsx
@@ -1,4 +1,8 @@
-import React from "react";
+import { useContext } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
+import GoBack from "../components/GoBack/Back";
+import Link from "next/link";
+import { useRouter } from 'next/router';
 
 // notifications/success?collection_id=1310744205&collection_status=approved&
 //payment_id=1310744205
@@ -7,24 +11,42 @@ import React from "react";
 //merchant_order_id=6472434290&preference_id=1216478638-7d90f482-e3f4-4451-aed1-03f6c1c0de49&site_id
 //=MLA&processing_mode=aggregator&merchant_account_id=null
 
-import { useRouter } from 'next/router';
 
-function Success() {
+const Success = () => {
+    const { theme } = useContext(ThemeContext);
     const router = useRouter();
-
-    const { payment_id, status, merchant_order_id } = router.query;
-    const query = router.query;
-
+    console.log("router: ", router)
+    console.log("router.query:", router.query)
+    // const { payment_id, status, merchant_order_id } = router.query;
+    // const query = router.query;
+    
     return (
         <div>
-            <h1>
-                <span>Pago aprobado!</span>
+        <GoBack path="/feed" />
+        <Link href="/feed">
+          <div
+            style={{
+              marginTop: "50%",
+              textAlign: "center",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <h1 className={theme ? "light_mode" : "dark_mode"}>
+                Pago aprobado!
             </h1>
-            <h2>Código de pago: {payment_id}</h2>
+            {/* <p className={theme ? "light_mode" : "dark_mode"}>
+              Próximamente disponible.
+            </p> */}
+            {/* <h2>Código de pago: {payment_id}</h2>
             <h2>Estado: {status}</h2>
             <h2>ID de la orden de compra: {merchant_order_id}</h2>
             <h3></h3>
-            <p>Ya puedes continuar navegando.</p>
+            <p>Ya puedes continuar navegando.</p> */}
+          </div>
+        </Link>
         </div>
     );
 }
