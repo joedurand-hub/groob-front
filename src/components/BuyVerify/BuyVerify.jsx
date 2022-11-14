b86f918e60a0ed3bb3485ed1d4fc9d95c0f28c95
Joel <joeljuliandurand@gmail.com>
2022-11-14 02:39:51 -0300

    verify

--- src/components/BuyVerify/BuyVerify.jsx
+++ src/components/BuyVerify/BuyVerify.jsx
@@ -22,9 +22,11 @@ const BuyVerify = ({ closeCardVerify, dataVerify }) => {
       postData: {
         idToVerify: dataVerify?._id,
         title: "Verificación de cuenta - Groob.",
+        price: 2999,
         descripcion: "Con la verificación de la cuenta obtenés el check azul en tu perfil. Además, en nuestro algoritmo damos múltiples beneficios a estos perfiles.",
         picURL: "https://tecnoartes.net/wordpress/wp-content/uploads/2021/02/check-icon-1.jpg",
         nombre: dataVerify?.firstName,
+        quantity: 1,
         apellido: dataVerify?.lastName,
         email: dataVerify?.email,
         // direccion,
