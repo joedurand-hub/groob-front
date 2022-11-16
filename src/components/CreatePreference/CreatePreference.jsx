b93722372feb6281a0980ea3403e32ad64c22937
Joel <joeljuliandurand@gmail.com>
2022-11-16 19:17:31 -0300

    createProduct preference

--- src/components/CreatePreference/CreatePreference.jsx
+++ src/components/CreatePreference/CreatePreference.jsx
@@ -3,31 +3,33 @@ import useAuthPost from "../../hooks/useAuthPost"
 import Button from "../Button/Button"
 import { ENDPOINT } from '../../helpers/constants'
 
-const CreatePreference = () => {
+const CreatePreference = ({accessToken, payer, price}) => {
     const {data, pending, error, sendData} = useAuthPost()
   
   const handlePreference = async () => {
-    // sendData({
-    //   endpoint: `${ENDPOINT}/preferenceProduct`,
-    //   postData: {
-    //     idToVerify: dataVerify?._id,
-    //     title: "Verificación de cuenta - Groob.",
-    //     price: 2999,
-    //     descripcion: "Con la verificación de la cuenta obtenés el check azul en tu perfil. Además, en nuestro algoritmo damos múltiples beneficios a estos perfiles.",
-    //     picURL: "https://tecnoartes.net/wordpress/wp-content/uploads/2021/02/check-icon-1.jpg",
-    //     nombre: dataVerify?.firstName,
-    //     quantity: 1,
-    //     apellido: dataVerify?.lastName,
-    //     email: dataVerify?.email,
-    //     // direccion,
-    //     // numeroDireccion,
-    //     // area,
-    //     // tel,
-    //     // postal,
-    //   },
-    // });
+    sendData({
+      endpoint: `${ENDPOINT}/preferenceProduct`,
+      postData: {
+        userId: payer?._id,
+        title: "Verificación de cuenta - Groob.",
+        price: price,
+        descripcion: "Con la verificación de la cuenta obtenés el check azul en tu perfil. Además, en nuestro algoritmo damos múltiples beneficios a estos perfiles.",
+        picURL: "https://tecnoartes.net/wordpress/wp-content/uploads/2021/02/check-icon-1.jpg",
+        nombre: payer?.firstName,
+        quantity: 9999,
+        apellido: payer?.lastName,
+        email: payer?.email,
+        // direccion,
+        // numeroDireccion,
+        // area,
+        // tel,
+        // postal,
+      },
+    });
   };
-
+  console.log("data:", data)
+  console.log("pending:", pending)
+  console.log("error:", error)
     return (
         <Button name="Comprar" variant="purchase" onClick={handlePreference}/>
   )
