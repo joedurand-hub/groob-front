62dfb741ca9587887f78f5d492dfb15ae402cfea
Joel <joeljuliandurand@gmail.com>
2022-11-15 23:48:23 -0300

    fix Slider

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -8,7 +8,7 @@ import { ThemeContext } from "../../contexts/ThemeContext";
 import { getCookie } from "cookies-next";
 
 const Slider = ({ allImages, userExplicitContent, nsfw, price }) => {
-  const token = getCookie("authtoken");
+  const token = getCookie("authtoken")
   const { theme } = useContext(ThemeContext);
   const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -23,72 +23,131 @@ const Slider = ({ allImages, userExplicitContent, nsfw, price }) => {
     setCurrentIndex(currentIndex === 0 ? -1 : currentIndex - 1);
   };
 
-  return (
-    <div
-      className={
-        theme
-          ? `${styles.container_slider} light_mode`
-          : `${styles.container_slider} dark_mode`
-      }
-    >
-      {currentIndex === 0 ? null : (
-        <button
-          onClick={previusImage}
-          className={`${styles.btn_slide} ${styles.prev}`}
-        >
-          <BsArrowLeft className={styles.icon} />
-        </button>
-      )}
 
-      {allImages &&
-        allImages.map((image, index) => {
-          let imageBlurred = buildUrl(`${image.public_id}`, {
-            cloud: {
-              cloudName: "groob",
-            },
-            transformations: {
-              effect: "blur:2000",
-              quality: 1,
-            },
-          });
-          return (
-            <div
-              key={index}
-              className={
-                currentIndex === index
-                  ? `${styles.slide} ${styles.active}`
-                  : `${styles.slide}`
-              }
-            >
-              {(userExplicitContent === false && nsfw === true) ||
-                (userExplicitContent === true && nsfw === true && price > 0) ||
-                (nsfw === true && price > 0) ||
-                (userExplicitContent === false && nsfw === true && price > 0) ||
-                (!token && nsfw === true && price > 0) ||
-                (!token && nsfw === true && <BlurredImage></BlurredImage>)}
-              {currentIndex === index && (
-                <Image
-                  key={index}
-                  src={imageBlurred}
-                  width={500}
-                  height={500}
-                  alt="Image"
-                />
-              )}
-            </div>
-          );
-        })}
+  if (userExplicitContent === false && nsfw === true 
+  || userExplicitContent === true && nsfw === true && price > 0
+  || nsfw === true && price > 0
+  || userExplicitContent === false && nsfw === true && price > 0
+  || !token && nsfw === true && price > 0
+  || !token && nsfw === true
+    ) {
+    return (
+      <div
+        className={
+          theme
+            ? `${styles.container_slider} light_mode`
+            : `${styles.container_slider} dark_mode`
+        }
+      >
+        {currentIndex === 0 ? null : (
+          <button
+            onClick={previusImage}
+            className={`${styles.btn_slide} ${styles.prev}`}
+          >
+            <BsArrowLeft className={styles.icon} />
+          </button>
+        )}
 
-      {currentIndex === quantity - 1 ? null : (
-        <button
-          onClick={nextImage}
-          className={`${styles.btn_slide} ${styles.next}`}
-        >
-          <BsArrowRight className={styles.icon} />
-        </button>
-      )}
-    </div>
-  );
+        {allImages &&
+          allImages.map((image, index) => {
+            let imageBlurred = buildUrl(`${image.public_id}`, {
+              cloud: {
+                cloudName: "groob",
+              },
+              transformations: {
+                effect: "blur:2000",
+                quality: 1,
+              },
+            });
+            return (
+              <div
+                key={index}
+                className={
+                  currentIndex === index
+                    ? `${styles.slide} ${styles.active}`
+                    : `${styles.slide}`
+                }
+              >
+                <BlurredImage>
+                </BlurredImage>
+                {currentIndex === index && (
+                  <Image
+                    key={index}
+                    src={imageBlurred}
+                    width={500}
+                    height={500}
+                    alt="Image"
+                  />
+                )}
+              </div>
+            );
+          })}
+
+        {currentIndex === quantity - 1 ? null : (
+          <button
+            onClick={nextImage}
+            className={`${styles.btn_slide} ${styles.next}`}
+          >
+            <BsArrowRight className={styles.icon} />
+          </button>
+        )}
+      </div>
+    );
+  } 
+  else {
+    return (
+      <div
+        className={
+          theme
+            ? `${styles.container_slider} light_mode`
+            : `${styles.container_slider} dark_mode`
+        }
+      >
+        {currentIndex === 0 ? null : (
+          <button
+            onClick={previusImage}
+            className={`${styles.btn_slide} ${styles.prev}`}
+          >
+            <BsArrowLeft className={styles.icon} />
+          </button>
+        )}
+
+        {allImages &&
+          allImages.map((image, index) => {
+            return (
+              <div
+                key={index}
+                className={
+                  currentIndex === index
+                    ? `${styles.slide} ${styles.active}`
+                    : `${styles.slide}`
+                }
+              >
+                {currentIndex === index && (
+                  <Image
+                    key={index}
+                    src={image.secure_url}
+                    layout="responsive"
+                    width={500}
+                    height={500}
+                    alt="Image"
+                  />
+                )}
+              </div>
+            );
+          })}
+
+        {currentIndex === quantity - 1 ? null : (
+          <button
+            onClick={nextImage}
+            className={`${styles.btn_slide} ${styles.next}`}
+          >
+            <BsArrowRight className={styles.icon} />
+          </button>
+        )}
+      </div>
+    );
+  }
 };
 
 export default Slider;
