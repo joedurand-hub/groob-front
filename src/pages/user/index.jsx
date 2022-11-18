5d18e4a1f580f7e33fc967d2a3d8d67fe6e33788
Joel <joeljuliandurand@gmail.com>
2022-11-18 20:47:37 -0300

    deploy

--- src/pages/user/index.jsx
+++ src/pages/user/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect } from "react";
 import { getCookie } from "cookies-next";
 import Layout from "../../components/Layout/Layout";
 import Profile from "../../components/Profile/Profile";
