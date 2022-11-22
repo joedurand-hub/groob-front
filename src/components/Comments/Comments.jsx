9713f360731aea61859140da50f9bd8259a4d706
Joel <joeljuliandurand@gmail.com>
2022-11-22 05:38:14 -0300

    ..

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,20 +1,30 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import styles from "./comments.module.css";
 
 const Comments = ({ allComments }) => {
-  const [colorComment, setColorComment] = useState("")
-  useEffect(() => {
-    const useGenerateRandomColor = () => {
-        setColorComment(Math.random().toString(16).substr(-6))
+  const [colorComment, setColorComment] = useState("");
+
+  const useGenerateRandomColor = () => {
+    setColorComment(Math.random().toString(16).substr(-6));
   };
-  if(allComments.length > 0) useGenerateRandomColor()
-  }, [])
+  
+  useEffect(() => {
+    if (allComments.length > 0) useGenerateRandomColor();
+  }, []);
+
   return (
     <div className={styles.container}>
       {allComments?.map((comment, index) => (
         <div className={styles.container_comments} key={index}>
-          <p  style={{color:`#${colorComment}`, margin: "5px", fontWeight: "700"}}>
-            {comment.userName}{": "}
+          <p
+            style={{
+              color: `#${colorComment}`,
+              margin: "5px",
+              fontWeight: "700",
+            }}
+          >
+            {comment.userName}
+            {": "}
           </p>
           <p className={styles.comment}>{comment.value}</p>
         </div>
