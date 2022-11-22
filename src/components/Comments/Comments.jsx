1310f35afce427a4723d6542846a755cf3b2c77e
Joel <joeljuliandurand@gmail.com>
2022-11-22 05:36:01 -0300

    colorComment

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -2,21 +2,18 @@ import {useState, useEffect} from "react";
 import styles from "./comments.module.css";
 
 const Comments = ({ allComments }) => {
-  const [color,setColor] = useState("")
+  const [colorComment, setColorComment] = useState("")
   useEffect(() => {
     const useGenerateRandomColor = () => {
-      const generateColor = () =>{
-          setColor(Math.random().toString(16).substr(-6))
-      }
-      generateColor()
+        setColorComment(Math.random().toString(16).substr(-6))
   };
-  useGenerateRandomColor()
+  if(allComments.length > 0) useGenerateRandomColor()
   }, [])
   return (
     <div className={styles.container}>
       {allComments?.map((comment, index) => (
         <div className={styles.container_comments} key={index}>
-          <p  style={{color:`#${color}`, margin: "5px", fontWeight: "700"}}>
+          <p  style={{color:`#${colorComment}`, margin: "5px", fontWeight: "700"}}>
             {comment.userName}{": "}
           </p>
           <p className={styles.comment}>{comment.value}</p>
